4f32d89ae6b2a00931b15be77e4ada6c2218a1d7
Carolina-Ceron <carol.ceron801@gmail.com>
2025-07-23 15:44:05 -0300

    portfolio-v1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-// Archivo JS base para futuras interacciones o animaciones
-
 document.addEventListener('DOMContentLoaded', () => {
     const elements = document.querySelectorAll('.slide-up-fade');
     elements.forEach((el, i) => {
         setTimeout(() => {
             el.classList.add('slide-up-fade-in');
-        }, 200 + i * 120); // delay escalonado
+        }, 200 + i * 120);
     });
 }); 
